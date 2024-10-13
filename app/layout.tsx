d9d089aa600e4bb8a03c6e4823dcfbf8a8c0e1cb
Benjy <123623302+brrock@users.noreply.github.com>
2024-10-13 20:24:02 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { ThemeProvider } from "@/components/custom/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  metadataBase: new URL("https://ai.ronotbroyt.xyz"),
+  title: "RoNotBroYT AI",
+  description: "Chatbot based on Google Gemini",
 };
 
 export default async function RootLayout({
